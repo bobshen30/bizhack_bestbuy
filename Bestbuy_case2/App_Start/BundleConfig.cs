9191723fdfa0c5cdf5cd854d87a6e331b656134d
Desta Chi <Desta>
2018-05-03 23:47:16 -0700

    initial commit

--- Bestbuy_case2/App_Start/BundleConfig.cs
+++ Bestbuy_case2/App_Start/BundleConfig.cs
@@ -20,11 +20,12 @@ namespace Bestbuy_case2
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/main.css",
                       "~/Content/site.css"));
         }
     }
